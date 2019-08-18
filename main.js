d743bf576d8c75525724b0fe36f1b35bfc17fee6
xuwenjian <chips.xu@magic-shield.com>
2019-08-18 18:30:54 +0800

    # init 1.0.0

---
+++ main.js
@@ -0,0 +1,8 @@
+const CommonUtils = require("./common_utils");
+const TimeUtils = require("./common_utils");
+const JsonUtils = require("./common_utils");
+const StringUtils = require("./common_utils");
+
+const VERSION = "1.0.0";
+
+module.exports = {CommonUtils, TimeUtils, JsonUtils, StringUtils, VERSION};
